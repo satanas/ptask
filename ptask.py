1b8cee62bd079c499a557408ac35b136f5fa09d1
Wil Alvarez <wil_alejandro@yahoo.com>
2011-02-22 18:52:47 -0430

    Fixed issues with database creation. Added 'admin' field to users. Implemented 'create' and 'list' commands for users

--- ptask.py
+++ ptask.py
@@ -1,70 +1,135 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 import cmd
 import getpass
 import random
 import sqlite3
 import hashlib
 
+DATABASE = 'ptask.db'
+
 INTRO = [
     'Welcome to ptask', 
     'Type "help" to get a list of available commands.',
 ]
 
-# FIXME: Esto no va aquí, debemos establecer la conexión al momento de autenticar al usuario
-#conn = sqlite3.connect('ptask.db')
-#c = conn.cursor()
+SQL_TASK_TABLE = '''CREATE TABLE taskrecords (
+    id INTEGER PRIMARY KEY, 
+    user_id INTEGER, 
+    description TEXT, 
+    task TEXT, 
+    start_date DATE, 
+    end_date DATE, 
+    estimated_time INTEGER, 
+    worked_time INTEGER, 
+    tags TEXT
+)'''
+
+SQL_USER_TABLE = '''CREATE TABLE users (
+    id INTEGER PRIMARY KEY, 
+    username TEXT,
+    realname TEXT, 
+    password_hash TEXT, 
+    password_salt TEXT, 
+    last_login DATE,
+    admin BOOLEAN
+) '''
+
+SQL_CREATE_USER = '''INSERT INTO users 
+    (username, realname, password_hash, password_salt, admin)
+    VALUES (?,?,?,?,?)'''
 
 class Ptask(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = 'ptask> '
         self.intro = '\n'.join(INTRO)
-        initdb()
+        self.__initdb()
         self.cmdloop()
-
-    def do_inituser(self, line):
-        #user_name, password
-        name = raw_input('user_name: ')
-        pwd = getpass.getpass("password: ")
-        repwd = getpass.getpass("password again: ")
-        if pwd == repwd:
-           create_user(name,pwd)
+    
+    def __validate_passwords(self, pwd1, pwd2):
+        if pwd1 == pwd2:
+            return True
         else:
-           print 'Password mismatch!'
+            return False
+        
+    def __generate_salt(self):
+        s = ""
+        for i in range(6):
+            s += chr(random.randrange(65,128))        
+        return s
+    
+    def __initdb(self):
+        if not os.path.isfile(DATABASE):
+            self.conn = sqlite3.connect(DATABASE)
+            cursor = self.conn.cursor()
+            cursor.execute(SQL_TASK_TABLE) 
+            cursor.execute(SQL_USER_TABLE)
+            self.conn.commit()
+            print "Database has been created. Now you need to create a user..."
+            self.__create_user(is_admin=True)
+            print ""
+        else:
+            self.conn = sqlite3.connect(DATABASE)
+        
+    def __create_user(self, is_admin=False):
+        realname = raw_input('Real name: ')
+        username = raw_input('Username: ')
+        
+        if is_admin:
+            admin = 1
+        else:
+            admin = raw_input('Admin? [y/n]: ').lower()
+            admin = 1 if (admin == 'y' or admin == '') else 0
+        
+        while 1:
+            pwd = getpass.getpass('Password: ')
+            repwd = getpass.getpass('Password again: ')
+            if self.__validate_passwords(pwd, repwd):
+                break
+            else:
+                print 'Password mismatch!. Try again'
+        
+        pwd_salt = self.__generate_salt()
+        m = hashlib.md5()
+        m.update(pwd + pwd_salt)
+        pwd_hash = m.hexdigest()
+        cursor = self.conn.cursor()
+        cursor.execute(SQL_CREATE_USER, (username, realname, pwd_hash, pwd_salt, admin))
+        self.conn.commit()
+        print "User created successfully!"
+        
+    def __list_users(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        print "%-20s %-20s %s\t %s" % ('Username', 'Real Name', 'Admin?', 'Last Login')
+        print "==============================================================="
+        for row in cursor:
+            admin = 'True' if row[6] == 1 else 'False'
+            print "%-20s %-20s %s\t\t %s" % (row[1], row[2], admin, row[5])
+    
+    #================================================
+    # Commands
+    #================================================
+    
+    def do_user(self, args):
+        if len(args.split()) < 1:
+            self.do_help()
+            return
+        
+        cmd = args.split()[0]
+        if cmd == 'create':
+            self.__create_user()
+        elif cmd == 'list':
+            self.__list_users()
 
-    def do_help(self, line):
+    def do_help(self, line=None):
         print "this is da help"
     
     def default(self, line):
         print '\n'.join(['Command not found.', INTRO[1]])
-
-
-    #class functions
-
-    def salt(self):
-        s = ""
-        for i in range(7):
-            s = s + chr(random.randrange(65,128))        
-        return s
-
-    def create_user(self, name, pwd):
-        m = hashlib.md5()
-        salt = self.salt()
-        m.update(password + self.salt())
-        password = m.hexdigest()
-        if self.c:
-           self.c.execute('INSERT INTO users VALUES (0, ?,?,?,null)', name, password, salt)
-           self.c.commit()
-
-    
-    def initdb(self):
-        conn = sqlite3.connect('ptask.db')
-        self.c = conn.cursor()
-        self.c.execute('''CREATE TABLE IF NOT EXIST taskrecords  (id INTEGER PRIMARY KEY, user_id INTEGER, description TEXT, task TEXT, start_date DATE, end_date DATE, estimated_time INTEGER, worked_timed INTEGER, tags TEXT)''') 
-        self.c.execute('''CREATE TABLE IF NOT EXIST users  (id INTEGER PRIMARY KEY, realname TEXT, password_hash TEXT, password_salt TEXT, last_login DATE) ''')
-        self.c.commit()
         
 if __name__ == '__main__':
     p = Ptask()
