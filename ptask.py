fa035a7709f1179c2b447cd1acae85a81bb58f3f
N3ro <venezuela.gtug@gmail.com>
2011-02-22 16:46:31 -0430

    User credentials mockup

--- ptask.py
+++ ptask.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import cmd
+import getpass
 import sqlite3
+import hashlib
 
 INTRO = [
     'Welcome to ptask', 
@@ -18,8 +20,19 @@ class Ptask(cmd.Cmd):
         cmd.Cmd.__init__(self)
         self.prompt = 'ptask> '
         self.intro = '\n'.join(INTRO)
- 	initdb()
         self.cmdloop()
+
+    def do_inituser(self, line):
+        #user_name, password
+        name = raw_input('user_name: ')
+        pwd = getpass.getpass("password: ")
+	repwd = getpass.getpass("password again: ")
+        if pwd == repwd:
+           m = hashlib.md5()
+           m.update(pwd)
+           self.user_name = name
+           self.user_pwd = m.hexdigest()
+           
 
     def do_help(self, line):
         print "this is da help"
@@ -27,10 +40,13 @@ class Ptask(cmd.Cmd):
     def default(self, line):
         print '\n'.join(['Command not found.', INTRO[1]])
 
-    def initdb(self):
+    def initdb(self, user_name, user_pwd, user_pwd_salt):
+        conn = sqlite3.connect('ptask.db')
+        c = conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXIST taskrecords  (id INTEGER PRIMARY KEY, user_id INTEGER, description TEXT, task TEXT, start_date DATE, end_date DATE, estimated_time INTEGER, worked_timed INTEGER, tags TEXT)''') 
         c.execute('''CREATE TABLE IF NOT EXIST users  (id INTEGER PRIMARY KEY, realname TEXT, password_hash TEXT, password_salt TEXT, last_login DATE) ''')
-        c.commit() 
+        c.commit()
+        c.execute('INSERT INTO users VALUES (0,?,?,?,null)', user_name, user_pwd, user_pwd_salt)
         
 if __name__ == '__main__':
     p = Ptask()
