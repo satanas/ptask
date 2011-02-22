125d174a0ee7a3240d207813628d6a6ed10e9803
N3ro <venezuela.gtug@gmail.com>
2011-02-22 15:18:45 -0430

    added initdb function

--- ptask.py
+++ ptask.py
@@ -9,7 +9,7 @@ INTRO = [
     'Type "help" to get a list of available commands.',
 ]
 
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('ptask.db')
 c = conn.cursor()
 
 class Ptask(cmd.Cmd):
@@ -17,6 +17,7 @@ class Ptask(cmd.Cmd):
         cmd.Cmd.__init__(self)
         self.prompt = 'ptask> '
         self.intro = '\n'.join(INTRO)
+ 	initdb()
         self.cmdloop()
 
     def do_help(self, line):
@@ -24,6 +25,11 @@ class Ptask(cmd.Cmd):
     
     def default(self, line):
         print '\n'.join(['Command not found.', INTRO[1]])
+
+    def initdb(self):
+        c.execute('''CREATE TABLE IF NOT EXIST taskrecords  (id INTEGER PRIMARY KEY, user_id INTEGER, description TEXT, task TEXT, start_date DATE, end_date DATE, estimated_time INTEGER, worked_timed INTEGER, tags TEXT)''') 
+        c.execute('''CREATE TABLE IF NOT EXIST users  (id INTEGER PRIMARY KEY, realname TEXT, password_hash TEXT, password_salt TEXT, last_login DATE) ''')
+        c.commit() 
         
 if __name__ == '__main__':
     p = Ptask()
