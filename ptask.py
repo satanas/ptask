b72a89b8fbea9f85a48ef0678fc085f730de8482
Wil Alvarez <wil_alejandro@yahoo.com>
2011-02-22 19:36:10 -0430

    Implemented exit routines and some help methods

--- ptask.py
+++ ptask.py
@@ -11,8 +11,14 @@ import hashlib
 DATABASE = 'ptask.db'
 
 INTRO = [
-    'Welcome to ptask', 
+    '\nWelcome to ptask', 
     'Type "help" to get a list of available commands.',
+]
+
+MISSING = [
+    'ERROR: Missing arguments',
+    'Type "help" for a list of available commands.',
+    'Type "help <command>" for a detailed help about the command'
 ]
 
 SQL_TASK_TABLE = '''CREATE TABLE taskrecords (
@@ -46,10 +52,14 @@ class Ptask(cmd.Cmd):
         cmd.Cmd.__init__(self)
         self.prompt = 'ptask> '
         self.intro = '\n'.join(INTRO)
-        self.__initdb()
-        self.cmdloop()
-    
-    def __validate_passwords(self, pwd1, pwd2):
+        self.admin = False
+        try:
+            self.__initdb()
+            self.cmdloop()
+        except KeyboardInterrupt:
+            self.do_exit('')
+        
+    def __confirm_passwords(self, pwd1, pwd2):
         if pwd1 == pwd2:
             return True
         else:
@@ -60,7 +70,7 @@ class Ptask(cmd.Cmd):
         for i in range(6):
             s += chr(random.randrange(65,128))        
         return s
-    
+        
     def __initdb(self):
         if not os.path.isfile(DATABASE):
             self.conn = sqlite3.connect(DATABASE)
@@ -70,9 +80,37 @@ class Ptask(cmd.Cmd):
             self.conn.commit()
             print "Database has been created. Now you need to create a user..."
             self.__create_user(is_admin=True)
-            print ""
         else:
             self.conn = sqlite3.connect(DATABASE)
+            self.__login()
+            
+    def __login(self):
+        while 1:
+            user = raw_input('Username: ')
+            pwd = getpass.getpass('Password: ')
+            if self.__validate_credentials(user, pwd):
+                break
+            else:
+                print 'Invalid credentials!. Try again'
+        
+    def __validate_credentials(self, user, pwd):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username=?", (user,))
+        row = cursor.fetchone()
+        
+        stored_hash = row[3]
+        pwd_salt = row[4]
+        admin = row[6]
+        
+        m = hashlib.md5()
+        m.update(pwd + pwd_salt)
+        pwd_hash = m.hexdigest()
+        
+        if pwd_hash != stored_hash:
+            return False
+        else:
+            self.admin = True if admin == 1 else False
+            return True
         
     def __create_user(self, is_admin=False):
         realname = raw_input('Real name: ')
@@ -87,7 +125,7 @@ class Ptask(cmd.Cmd):
         while 1:
             pwd = getpass.getpass('Password: ')
             repwd = getpass.getpass('Password again: ')
-            if self.__validate_passwords(pwd, repwd):
+            if self.__confirm_passwords(pwd, repwd):
                 break
             else:
                 print 'Password mismatch!. Try again'
@@ -116,7 +154,7 @@ class Ptask(cmd.Cmd):
     
     def do_user(self, args):
         if len(args.split()) < 1:
-            self.do_help()
+            print '\n'.join(MISSING)
             return
         
         cmd = args.split()[0]
@@ -125,11 +163,34 @@ class Ptask(cmd.Cmd):
         elif cmd == 'list':
             self.__list_users()
 
-    def do_help(self, line=None):
-        print "this is da help"
-    
+    #def do_help(self, line=None):
+    #    print "this is da help"
+    def do_EOF(self, line):
+        return self.do_exit(line)
+        
+    def do_exit(self, line):
+        print "Bye"
+        return True
+        
     def default(self, line):
         print '\n'.join(['Command not found.', INTRO[1]])
+        
+    def help_user(self):
+        print '\n'.join(['Handles user operations',
+            'USAGE: user <arg>',
+            '  <arg>:',
+            '    create - Create a new user',
+            '    list - Show you a list of all users',
+        ])
+        
+    def help_help(self):
+        print 'Show this help'
+        
+    def help_exit(self):
+        print 'Duh! Exit'
+    
+    def help_EOF(self):
+        print 'Duh! Exit'
         
 if __name__ == '__main__':
     p = Ptask()
